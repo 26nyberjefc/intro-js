c11f6d8a4ca136229d114eaa76ed7f358b5f06a4
26nyberjefc <26nyberjefc@washk12.org>
2025-02-13 11:44:19 -0700

    2

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
 const prompt = require('prompt-sync')();
-
-// Get user input for character name and game
-let characterName = prompt("What is your name? ");
-let gameName = prompt("What's your favorite game? ");
-
-// Combine the inputs into a greeting
-let greeting = "Welcome to " + gameName + ", " + characterName + "! Prepare for your adventure!";
-
-console.log(greeting);
-
-let cardSuit = prompt("What is your card suit? ");
-let cardRank = prompt("What's your card rank? ");
-let pickCard = "Your card is the " + cardRank + " of " + cardSuit + "!";
-console.log(pickCard)
+let name = prompt("What's your name? ");
+console.log(`Hello, ${name}!`);
